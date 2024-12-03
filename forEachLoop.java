2de653fc256297e41bb89b4b47305c85750eb387
Sachchidanandj16 <158037494+Sachchidanandj16@users.noreply.github.com>
2024-12-04 01:17:53 +0530

    Update and rename foeeachj.java to forEachLoop.java

--- forEachLoop.java
+++ forEachLoop.java
@@ -1,4 +1,5 @@
-public class foeeachj {
+//this code is to understand ForEach loop in java
+public class forEachLoop {
     public static void main(String[] args) {
         String[] A= {"cat","Dog","Aman" };
         for (String i : A) {
