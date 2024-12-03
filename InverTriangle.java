e973f5363003d26930a2fffc56e0e9f425042f44
Sachchidanandj16 <158037494+Sachchidanandj16@users.noreply.github.com>
2024-12-04 01:15:39 +0530

    Update and rename Invertria.java to InverTriangle.java

--- InverTriangle.java
+++ InverTriangle.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Invertria {
+public class InverTriangle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter number");
