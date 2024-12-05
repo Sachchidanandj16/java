b790a50b96e01dcf34d6de6a7fabf07b72a659b2
Sachchidanand Jha <158037494+Sachchidanandj16@users.noreply.github.com>
2024-12-05 20:50:28 +0530

    Update and rename lcm.java to LCM.java

--- LCM.java
+++ LCM.java
@@ -1,5 +1,6 @@
+//Find LCM of given 2 Number given by User
 import java.util.Scanner;
-public class lcm{
+public class LCM{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter 1st Number");
